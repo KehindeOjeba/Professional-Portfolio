a3f6f45f15dec111548599832a307804b4afe787
kestopher <kennykestopher@gmail.com>
2025-09-01 21:04:22 +0100

    update

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -3,15 +3,6 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectLabel,
-  SelectTrigger,
-  SelectValue
-} from "@/components/ui/select";
 import {FaPhoneAlt, FaEnvelope, FaMapMarkerAlt} from 'react-icons/fa'
 import { motion } from "framer-motion";
 
@@ -24,7 +15,7 @@ const info = [
   {
     icon: <FaEnvelope/> ,
     title: 'Email',
-    description: 'kenny.r@gmail.com' 
+    description: 'kennyojeba.r@gmail.com' 
   },
   {
     icon: <FaMapMarkerAlt/> ,
